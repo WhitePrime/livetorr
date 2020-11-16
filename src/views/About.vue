67a7f9dacc9d63f1813370d2873dc0e4db62c004
White Prime <WhitePrime@users.noreply.github.com>
2020-11-16 08:27:53 +0300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,31 +7,10 @@
         <p>Live Torrent created to explore, share and play media of torrent files online.</p>
         <p>
           Live Torrent is open source.
-          <br />Visit the github repository from
-          <a href="https://github.com/Davenchy/live-torrent">here</a>.
+          <br />
         </p>
-        <p></p>
-        <p>My twitter user: @fadi_davenchy.</p>
-        <p>
-          Backend is based on
-          <a href="https://webtorrent.io">webtorrent</a> project.
-        </p>
-        <p>
-          Video Player is based on
-          <a href="https://plyr.io">Plyr.js</a>.
-        </p>
-        <p>
-          Live Torrent is a
-          <a href="https://vuejs.org/">vue.js</a> project.
-        </p>
-        <p>
-          Movies from
-          <a href="https://yts.am">yts</a>.
-        </p>
-        <p>
-          Subtitles from
-          <a href="https://opensubtitles.org">opensubtitles.org</a>.
-        </p>
+        <p><a href="https://ttttt.me/ITHARD">Telegram channel</a></p>
+        
       </v-flex>
     </v-layout>
   </v-container>
